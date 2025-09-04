ab9dadc370de8efc9ed04b8eeb062d332a45d7f7
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-04 11:07:59 +0530

    simple http endpoints added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,4 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+    
+})
+
+app.post("/room", (req, res) => {
+    
+})
+
 app.listen(3000);
